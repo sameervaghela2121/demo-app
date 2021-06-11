667036cd18438287cf4f0546f1e17f50ef00b644
aglowid-pro-1 <sameervaghela21@gmail.com>
2021-06-11 12:12:34 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculater from './Components/Calculater';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculater/>
+      
+    </div>
+  );
+}
+
+export default App;
